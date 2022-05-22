1aec30091d2b352632e0ceb495236e6d14893088
cty3000 <1735150+cty3000@users.noreply.github.com>
2022-05-22 21:35:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-curl -LO https://go.dev$(curl -s https://go.dev/dl/ \
+GOTARGZ=$(curl -s https://go.dev/dl/ \
   | egrep "go[0-9].[0-9]+.[0-9]+.linux-arm64.tar.gz" \
-  | sed -e 's/.*\(\/dl\/go[0-9].[0-9]\+.[0-9]\+.linux-arm64.tar.gz\).*/\1/g' \
+  | sed -e 's/.*\(go[0-9].[0-9]\+.[0-9]\+.linux-arm64.tar.gz\).*/\1/g' \
   | head -n1)
+
+curl -LO https://go.dev/dl/$GOTARGZ
+
+sudo tar -C /usr/local -xzf $GOTARGZ
