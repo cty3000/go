d1bf86465ac3b2187fabe722245bd4e99bc96894
cty3000 <1735150+cty3000@users.noreply.github.com>
2022-05-22 21:30:36 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -LO https://go.dev$(curl -s https://go.dev/dl/ \
+  | egrep "go[0-9].[0-9]+.[0-9]+.linux-arm64.tar.gz" \
+  | sed -e 's/.*\(\/dl\/go[0-9].[0-9]\+.[0-9]\+.linux-arm64.tar.gz\).*/\1/g' \
+  | head -n1)
